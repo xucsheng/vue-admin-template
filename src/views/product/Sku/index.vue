90dc7be20e1a43a0532301183e93bed511113738
xuchunsheng <xuchunsheng123>
2023-02-20 17:21:48 +0800

    后台管理sku列表显示和静态页面，xu_csheng 20230220

--- src/views/product/Sku/index.vue
+++ src/views/product/Sku/index.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
     <!--表格-->
-    <el-table style="width:100%" border>
+    <el-table style="width:100%" border :data="records">
       <el-table-column  type="index" prop="prop" label="序号" width="80" align="center"></el-table-column>
-      <el-table-column  prop="prop" label="名称" width="width"></el-table-column>
-      <el-table-column  prop="prop" label="描述" width="width"></el-table-column>
-      <el-table-column  prop="prop" label="默认图片" width="110"></el-table-column>
-      <el-table-column  prop="prop" label="重量" width="80"></el-table-column>
-      <el-table-column  prop="prop" label="价格" width="80"></el-table-column>
-      <el-table-column  prop="prop" label="操作" width="width"></el-table-column>
+      <el-table-column  prop="skuName" label="名称" width="width"></el-table-column>
+      <el-table-column  prop="skuDesc" label="描述" width="width"></el-table-column>
+      <el-table-column  label="默认图片" width="110">
+        <template v-slot="{row,$index}">
+          <img :src="row.skuDefaultImg" style="width: 80px;height: 80px"/>
+        </template>
+      </el-table-column>
+      <el-table-column  prop="weight" label="重量" width="80"></el-table-column>
+      <el-table-column  prop="price" label="价格" width="80"></el-table-column>
+      <el-table-column  prop="prop" label="操作" width="width">
+        <template v-slot="{row,$index}">
+          <el-button type="success" icon="el-icon-sort-down" size="mini"></el-button>
+          <el-button type="success" icon="el-icon-sort-up" size="mini"></el-button>
+          <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+          <el-button type="primary" icon="el-icon-info" size="mini"></el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+        </template>
+
+      </el-table-column>
     </el-table>
     <!--分页器-->
     <el-pagination
@@ -18,10 +31,11 @@
       :page-size="limit"
       :page-count="7"
       :page-sizes="[3,5,10]"
-      layout="prev, pager, next,jumper,->,sizes,total">
+      layout="prev, pager, next,jumper,->,sizes,total"
+      @current-change="getSkuList"
+      @size-change="handleSizeChange"
+    >
     </el-pagination>
-    <!--      @current-change="getPageList"-->
-    <!--      @size-change="handleSizeChange"-->
 
   </div>
 </template>
@@ -31,10 +45,33 @@ export default {
   data(){
     return{
       page:1,
-      total:20,
+      total:0,
       limit:10,
+      records:[],
     }
-  }
+  },
+  mounted() {
+    this.getSkuList();
+  },
+  methods:{
+    async getSkuList(pages =1){
+      this.page = pages;
+      const{page,limit}=this;
+      let result =  await this.$API.sku.reqSkuList(page,limit);
+      if(result.code == 200){
+        this.records = result.data.records;
+        this.page = result.data.current;
+        this.total = result.data.total;
+        this.limit = result.data.size;
+      }
+    },
+    handleSizeChange(limit){
+      this.limit = limit;
+      this.getSkuList();
+    },
+
+  },
+
 }
 
 
