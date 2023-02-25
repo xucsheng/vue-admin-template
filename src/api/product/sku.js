225e5e34bc0477a7f28152b1bd6c8676a028279a
xuchunsheng <xuchunsheng123>
2023-02-25 09:40:47 +0800

    后台管理sku显示详情开发完成，xu_csheng 20230225

--- src/api/product/sku.js
+++ src/api/product/sku.js
@@ -3,3 +3,32 @@ import request from '@/utils/request'
 
 // GET /admin/product/list/{page}/{limit}
 export const reqSkuList=(page,limit)=>request({url:`/admin/product/list/${page}/${limit}`,method:'get'});
+
+// 上架  GET /admin/product/onSale/{skuId}
+export const reqSale=(skuId)=>request({url:`/admin/product/onSale/${skuId}`,method:'get'});
+
+// 下架 GET /admin/product/cancelSale/{skuId}
+export const reqCancelSale=(skuId)=>request({url:`/admin/product/cancelSale/${skuId}`,method:'get'});
+
+// 获取sky详情的接口 GET /admin/product/getSkuById/{skuId}
+export const reqSkuById=(skuId)=>request({url:`/admin/product/getSkuById/${skuId}`,method:'get'});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/views/product/Sku/index.vue
+++ src/views/product/Sku/index.vue
@@ -14,10 +14,10 @@
       <el-table-column  prop="price" label="价格" width="80"></el-table-column>
       <el-table-column  prop="prop" label="操作" width="width">
         <template v-slot="{row,$index}">
-          <el-button type="success" icon="el-icon-sort-down" size="mini"></el-button>
-          <el-button type="success" icon="el-icon-sort-up" size="mini"></el-button>
-          <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-          <el-button type="primary" icon="el-icon-info" size="mini"></el-button>
+          <el-button v-if="row.isSale == 0"  type="success" icon="el-icon-sort-down" size="mini" @click="sale(row)"></el-button>
+          <el-button  v-else type="success" icon="el-icon-sort-up" size="mini" @click="cancelSale(row)"></el-button>
+          <el-button type="primary" icon="el-icon-edit" size="mini" @click="edit(row)"></el-button>
+          <el-button type="primary" icon="el-icon-info" size="mini" @click="getSkuInfo(row)"></el-button>
           <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
         </template>
 
@@ -36,6 +36,44 @@
       @size-change="handleSizeChange"
     >
     </el-pagination>
+    <!--抽屉效果-->
+    <el-drawer
+      title="我是标题"
+      :visible.sync="show"
+      size="50%"
+      :show-close="false">
+      <el-row>
+        <el-col :span="5">名称</el-col>
+        <el-col :span="16">{{skuInfo.skuName}}</el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">描述</el-col>
+        <el-col :span="16">{{skuInfo.skuDesc}}</el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">价格</el-col>
+        <el-col :span="16">{{skuInfo.price}}元</el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">平台属性</el-col>
+        <el-col :span="16">
+          <template >
+              <el-tag type="success" v-for="(attr,index) in skuInfo.skuAttrValueList" :key="attr.id" style="margin-right: 10px;">{{attr.attrId}}-{{attr.valueId}}</el-tag>
+          </template>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">商品图片</el-col>
+        <el-col :span="16">
+          <el-carousel height="150px">
+            <el-carousel-item v-for="item in skuInfo.skuImageList" :key="item.id">
+              <img :src="item.imgUrl"/>
+            </el-carousel-item>
+          </el-carousel>
+
+        </el-col>
+      </el-row>
+    </el-drawer>
 
   </div>
 </template>
@@ -48,6 +86,9 @@ export default {
       total:0,
       limit:10,
       records:[],
+      skuInfo:[], // 存储sku的信息
+      show:false,
+
     }
   },
   mounted() {
@@ -69,6 +110,35 @@ export default {
       this.limit = limit;
       this.getSkuList();
     },
+    // 上架
+   async sale(row){
+     let result = await this.$API.sku.reqSale(row.id);
+     if(result.code==200){
+       row.isSale = 1;
+       this.$message({type:'success',message:'上架成功'});
+       this.getSkuList();
+     }
+
+    },
+   async cancelSale(row){
+     let result = await this.$API.sku.reqCancelSale(row.id);
+      if(result.code==200){
+        row.isSale = 0;
+        this.$message({type:'success',message:'下架成功'});
+        this.getSkuList();
+      }
+    },
+   async edit(sku){
+      this.$message({type:'success',message:'正在开发中'});
+    },
+    async getSkuInfo(sku){
+      this.show = true;
+      // 获取sku的数据
+      let result = await this.$API.sku.reqSkuById(sku.id);
+      if(result.code==200){
+        this.skuInfo = result.data;
+      }
+    },
 
   },
 
@@ -76,6 +146,39 @@ export default {
 
 
 </script>
-<style>
+<style >
+
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
+}
+
+.el-carousel__button{
+   width: 10px;
+   height: 10px;
+   background: red;
+   border-radius: 50%;
+ }
+</style>
+
+<style scoped>
+  .el-row .el-col-5{
+     font-size: 18px;
+     text-align: right;
+   }
+  .el-col{
+    margin: 10px 10px;
+    }
 
 </style>
